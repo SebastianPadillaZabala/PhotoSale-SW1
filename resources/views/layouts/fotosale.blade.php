f3223efa69da351866edf42a64eb1708f95ee979
Sebastian Padilla <70664928+SebastianPadillaZabala@users.noreply.github.com>
2022-10-11 17:50:05 -0400

    fsangite

--- resources/views/layouts/fotosale.blade.php
+++ resources/views/layouts/fotosale.blade.php
@@ -18,10 +18,10 @@
           </div>
           <p class="text-sm font-medium text-gray-900">$10</p>
         </div>
-      </div>
-      <a href="{{route('planes')}}">
-          <button class="mt-4 text-xl w-28 text-white bg-green-500 hover:bg-green-600 py-2 rounded-xl shadow-lg">Subir foto</button>
+        <a href="#">
+          <button class="mt-4 text-xl w-28 text-white bg-green-500 hover:bg-green-600 py-2 rounded-xl shadow-lg">Comprar</button>
         </a>
+      </div>
     </div>
   </div>
 </div>
