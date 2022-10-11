2457f32ebe86c93ff6da6047114f5d4d1ab5893f
Sebastian Padilla <70664928+SebastianPadillaZabala@users.noreply.github.com>
2022-10-11 17:32:35 -0400

    feads

--- resources/views/layouts/fotosale.blade.php
+++ resources/views/layouts/fotosale.blade.php
@@ -17,10 +17,10 @@
             </h3>
           </div>
           <p class="text-sm font-medium text-gray-900">$10</p>
-          <a href="#">
+        </div>
+        <a href="#">
             <button class="mt-4 text-xl w-28 text-white bg-green-500 hover:bg-green-600 py-2 rounded-xl shadow-lg">Comprar</button>
           </a>
-        </div>
       </div>
     </div>
   </div>
