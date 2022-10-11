1276327f34f99c5621d61cd911e62ad88821d08e
Sebastian Padilla <70664928+SebastianPadillaZabala@users.noreply.github.com>
2022-10-11 17:51:43 -0400

    fsangitee

--- resources/views/layouts/fotosale.blade.php
+++ resources/views/layouts/fotosale.blade.php
@@ -6,7 +6,7 @@
     <h2 class="text-2xl font-bold tracking-tight text-gray-900">Foto en la que apareces</h2>
     <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
       <div class="group relative">
-        <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 hover:opacity-75 lg:aspect-none lg:h-80">
+        <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
           <img src="{{Storage::disk('s3')->url($path)}}" class="h-full w-full object-cover object-center lg:h-full lg:w-full">
         </div>
         <div class="mt-4 flex justify-between">
@@ -15,12 +15,12 @@
               <span aria-hidden="true" class="absolute inset-0"></span>
               Precio
             </h3>
+            <a href="#">
+          <button class="mt-4 text-xl w-28 text-white bg-green-500 hover:bg-green-600 py-2 rounded-xl shadow-lg">Comprar</button>
+        </a>
           </div>
           <p class="text-sm font-medium text-gray-900">$10</p>
         </div>
-        <a href="#">
-          <button class="mt-4 text-xl w-28 text-white bg-green-500 hover:bg-green-600 py-2 rounded-xl shadow-lg">Comprar</button>
-        </a>
       </div>
     </div>
   </div>
