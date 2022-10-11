484625ae6225269ae9c707ebfb17b32499a26e85
Sebastian Padilla <70664928+SebastianPadillaZabala@users.noreply.github.com>
2022-10-11 07:52:19 -0400

    fe3

--- resources/views/fotografo/fotosale.blade.php
+++ resources/views/fotografo/fotosale.blade.php
@@ -8,8 +8,8 @@
       <button class="mt-4 text-xl w-28 text-white bg-green-500 hover:bg-green-600 py-2 rounded-xl shadow-lg">Subir foto</button>
     </a>
     <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-    @foreach($fotos as $f)
       <div class="group relative">
+      @foreach($fotos as $f)
         <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
           <img src="{{Storage::disk('s3')->url($f->path)}}" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
         </div>
