9e5a4954982d27bc70db97514a322cc8aa1fabf2
Sebastian Padilla <70664928+SebastianPadillaZabala@users.noreply.github.com>
2022-10-11 17:27:24 -0400

    fe111

--- resources/views/layouts/fotosale.blade.php
+++ resources/views/layouts/fotosale.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="bg-white">
   <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Foto en la que apareces</h2>
     <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
       <div class="group relative">
         <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
@@ -15,7 +15,7 @@
                 <span aria-hidden="true" class="absolute inset-0"></span>
                 Precio
             </h3>
-            <a href="">
+            <a href="#">
             <p class="mt-1 text-sm text-gray-500">Comprar</p>
             </a>
           </div>
