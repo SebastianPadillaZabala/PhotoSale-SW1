fd512fa798ebcad9c3272b8287c54c3fbf59ac38
Sebastian Padilla <70664928+SebastianPadillaZabala@users.noreply.github.com>
2022-10-11 17:40:27 -0400

    fsangitaa

--- resources/views/layouts/fotosale.blade.php
+++ resources/views/layouts/fotosale.blade.php
@@ -5,7 +5,7 @@
   <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
     <h2 class="text-2xl font-bold tracking-tight text-gray-900">Foto en la que apareces</h2>
     <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-      <div class="group static">
+      <div class="group relative">
         <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
           <img src="{{Storage::disk('s3')->url($path)}}" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
         </div>
@@ -18,9 +18,9 @@
           </div>
           <p class="text-sm font-medium text-gray-900">$10</p>
         </div>
-        <a href="#">
-      <button class="mt-4 text-xl w-28 text-white bg-green-500 hover:bg-green-600 py-2 rounded-xl shadow-lg">Subir foto</button>
-    </a>
+        <a href="">
+          <button class="mt-4 text-xl w-28 text-white bg-green-500 hover:bg-green-600 py-2 rounded-xl shadow-lg">Subir foto</button>
+        </a>
       </div>
     </div>
   </div>
