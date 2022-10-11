819b2ef12ce9ecf2a5fa52006f650edc9bb55d2c
Sebastian Padilla <70664928+SebastianPadillaZabala@users.noreply.github.com>
2022-10-11 07:48:50 -0400

    fe

--- resources/views/fotografo/fotosale.blade.php
+++ resources/views/fotografo/fotosale.blade.php
@@ -7,11 +7,11 @@
     <a href="{{route('createFoto', [$id])}}">
       <button class="mt-4 text-xl w-28 text-white bg-green-500 hover:bg-green-600 py-2 rounded-xl shadow-lg">Subir foto</button>
     </a>
-
+  @foreach($fotos as $f)
     <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
       <div class="group relative">
         <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-          <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+          <img src="{{Storage::disk('s3')->url($f->path)}}" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
         </div>
         <div class="mt-4 flex justify-between">
           <div>
@@ -23,11 +23,11 @@
             </h3>
             <p class="mt-1 text-sm text-gray-500">Black</p>
           </div>
-          <p class="text-sm font-medium text-gray-900">$35</p>
+          <p class="text-sm font-medium text-gray-900">$10</p>
         </div>
       </div>
     </div>
+    @endforeach
   </div>
 </div>
-
 @endsection
